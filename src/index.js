82f092da2be0b46762dbeda3dd0b2be3fb05752a
Hoornito <agus.fernandez.y@gmail.com>
2022-06-12 04:42:18 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { configureStore } from '@reduxjs/toolkit';
+import {createRoot} from 'react-dom/client';
+
+import { Provider } from 'react-redux'
+
+import rootReducer from './redux/reducers'
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/theme.css'
+import './assets/css/index.css'
+
+import Layout from './components/layout/Layout'
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+const store = configureStore({reducer:{rootReducer}});
+document.title = 'Front'
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+      <React.StrictMode>
+        <Layout />
+      </React.StrictMode>
+  </Provider>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

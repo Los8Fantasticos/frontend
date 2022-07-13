5abdb595fe5bae946915e5e2d55b25ecdb8503d0
Hoornito <agus.fernandez.y@gmail.com>
2022-07-13 18:48:07 -0300

    jeje

--- src/index.js
+++ src/index.js
@@ -1,34 +1,15 @@
 import React from 'react';
-import reportWebVitals from './reportWebVitals'
+import ReactDOM from 'react-dom';
 
-import { configureStore } from '@reduxjs/toolkit';
-import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
-import { Provider } from 'react-redux'
-
-import rootReducer from './redux/reducers'
-
-import './assets/boxicons-2.0.7/css/boxicons.min.css'
-import './assets/css/grid.css'
-import './assets/css/theme.css'
-import './assets/css/index.css'
-
-import Layout from './components/layout/Layout'
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-const store = configureStore({reducer:{rootReducer}});
-document.title = 'Front'
-root.render(
-  <Provider store={store}>
-      <React.StrictMode>
-        <Layout />
-      </React.StrictMode>
-  </Provider>
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,56 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
 
-import Table from '../components/table/Table'
-
-import customerList from '../assets/JsonData/customers-list.json'
-
-const customerTableHead = [
-    '',
-    'name',
-    'email',
-    'phone',
-    'total orders',
-    'total spend',
-    'location'
-]
-
-const renderHead = (item, index) => <th key={index}>{item}</th>
-
-const renderBody = (item, index) => (
-    <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
-        <td>{item.email}</td>
-        <td>{item.phone}</td>
-        <td>{item.total_orders}</td>
-        <td>{item.total_spend}</td>
-        <td>{item.location}</td>
-    </tr>
-)
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
-    return (
-        <div>
-            <h2 className="page-header">
-                customers
-            </h2>
-            <div className="row">
-                <div className="col-12">
-                    <div className="card">
-                        <div className="card__body">
-                            <Table
-                                limit='10'
-                                headData={customerTableHead}
-                                renderHead={(item, index) => renderHead(item, index)}
-                                bodyData={customerList}
-                                renderBody={(item, index) => renderBody(item, index)}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const selectionsettings = { persistSelection: true };
+  const toolbarOptions = ['Delete'];
+  const editing = { allowDeleting: true, allowEditing: true };
 
-export default Customers
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        enableHover={false}
+        allowPaging
+        pageSettings={{ pageCount: 5 }}
+        selectionSettings={selectionsettings}
+        toolbar={toolbarOptions}
+        editSettings={editing}
+        allowSorting
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Customers;
